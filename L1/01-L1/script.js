db54d99f6ba4fc0e5399bd9754ec401a20252393
alexandrebpetri <alexandrepetri.dev@gmail.com>
2025-03-27 09:57:43 -0300

    27-03-2025

---
+++ L1/01-L1/script.js
@@ -0,0 +1,8 @@
+// Função para alterar a cor de fundo
+document.getElementById("btnAzul").addEventListener("click", function() {
+    document.body.style.backgroundColor = "blue";
+});
+
+document.getElementById("btnVerde").addEventListener("click", function() {
+    document.body.style.backgroundColor = "green";
+});
